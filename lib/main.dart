1b819c9ca0100ee742f82db345f87a03c1e13712
Ming <n.gihming@yahoo.com>
2022-02-19 20:58:10 +0800

    Greetings page pending

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ import 'package:handsfree/services/auth.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'package:handsfree/models/newUser.dart';
+import 'theme/theme_manager.dart';
+import 'theme/theme_constants.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,6 +20,8 @@ void main() async {
 
   runApp(const MyApp());
 }
+
+ThemeManager _themeManager = ThemeManager();
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -30,23 +34,26 @@ class MyApp extends StatelessWidget {
       initialData: null,
       child: MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
+        // theme: ThemeData(
+        //   // This is the theme of your application.
+        //   //
+        //   // Try running your application with "flutter run". You'll see the
+        //   // application has a blue toolbar. Then, without quitting the app, try
+        //   // changing the primarySwatch below to Colors.green and then invoke
+        //   // "hot reload" (press "r" in the console where you ran "flutter run",
+        //   // or simply save your changes to "hot reload" in a Flutter IDE).
+        //   // Notice that the counter didn't reset back to zero; the application
+        //   // is not restarted.
+        //   primarySwatch: Colors.blue,
+        // ),
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: _themeManager.themeMode,
         home: const Wrapper(),
         routes: {
           "/auth/signIn": (context) => const SignIn(),
           "/auth/signUp": (context) => const SignUp(),
-          "/home":(context) => const Home(),
+          "/home": (context) => const Home(),
         },
       ),
     );
---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+//Colors
+// const COLOR_BLACK
