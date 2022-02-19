9a873c6f9f9637212728b04bf3484ec99351a269
Ming <n.gihming@yahoo.com>
2022-02-19 20:58:37 +0800

    Greeting page pending

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:handsfree/screens/authenticate/signIn.dart';
-
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:simple_shadow/simple_shadow.dart';
 import '../../services/auth.dart';
 
 class Authenticate extends StatelessWidget {
@@ -11,31 +12,68 @@ class Authenticate extends StatelessWidget {
     final AuthService _auth = AuthService();
 
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          const Text('Authenticate page'),
-          Row(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/auth/signIn');
-                  },
-                  child: const Text('Sign In')),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/auth/signUp');
-                  },
-                  child: const Text('Sign Up')),
-              ElevatedButton(
-                  onPressed: () {
-                    _auth.signInWithGoogle();
-                  },
-                  child: const Text('Sign In with Google'))
-            ],
-          ),
-        ],
-      ),
-    ));
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          // clipBehavior: Clip.antiAliasWithSaveLayer,
+          // overflow: Overflow.visible,
+          children: <Widget>[
+            Positioned(
+              left: 20,
+              // top: 20,
+              child: Container(
+                // width: 400,
+                // height: 300,
+                alignment: Alignment.topRight,
+                child: SvgPicture.asset(
+                  'assets/purple.svg',
+                ),
+              ),
+            ),
+            Positioned(
+              // left: 20,
+              // top: 20,
+              child: Container(
+                // width: 400,
+                // height: 300,
+                alignment: Alignment.topRight,
+                child: SvgPicture.asset(
+                  'assets/yellow.svg',
+                ),
+              ),
+            ),
+            Container(
+              // width: 400,
+              // height: 300,
+              alignment: Alignment.topCenter,
+              child: SvgPicture.asset(
+                'assets/magenta.svg',
+                fit: BoxFit.fill,
+              ),
+            ),
+            const Text('Authenticate page'),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/auth/signIn');
+                    },
+                    child: const Text('Sign In')),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/auth/signUp');
+                    },
+                    child: const Text('Sign Up')),
+                ElevatedButton(
+                    onPressed: () {
+                      _auth.signInWithGoogle();
+                    },
+                    child: const Text('Sign In with Google')),
+              ],
+            ),
+          ],
+        ), //Stack
+      ), //Center
+    );
   }
 }
