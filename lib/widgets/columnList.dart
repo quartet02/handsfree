37a4b052da1a9bc2a7908a6eb76abb714f045754
Ming <n.gihming@yahoo.com>
2022-02-24 01:11:38 +0800

    Sublevel

---
+++ lib/widgets/columnList.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:handsfree/screens/learn/sublevel.dart';
+import 'package:handsfree/utils/provider/lessonProvider.dart';
+import 'package:provider/provider.dart';
+
+import '../models/lessonModel.dart';
+import '../utils/constants.dart';
+
+class ColumnList extends StatelessWidget {
+  final LessonModel lesson;
+
+  ColumnList({required this.lesson});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<LessonProvider>(builder: (context, lessoned, child) {
+      return GestureDetector(
+        onTap: () {
+          lessoned.setClickLesson = lesson;
+          print("$lesson is sohai");
+          Navigator.pushNamed(context, "/sublevel");
+        },
+        child: Container(
+          height: 100,
+          width: 200,
+          child: Row(
+            children: <Widget>[
+              Container(
+                  child: Image.asset(
+                lesson.lessonImage,
+                scale: 4,
+              )),
+              const Padding(padding: EdgeInsets.only(right: 10)),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(padding: EdgeInsets.only(top: 20)),
+                  Text(
+                    lesson.lessonName,
+                    style: GoogleFonts.montserrat(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: kText,
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.only(top: 5)),
+                  Text(
+                    lesson.lessonDesc,
+                    style: GoogleFonts.montserrat(
+                      fontSize: 12.8,
+                      fontWeight: FontWeight.w400,
+                      color: kText,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    });
+  }
+}
