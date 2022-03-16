fa79a5d14c9b38c6f61b2a7e6d12dc4710b1a1fd
Ming <n.gihming@yahoo.com>
2022-03-16 22:58:34 +0800

    minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:handsfree/provider/helpdeskProvider.dart';
-import 'package:handsfree/provider/lessonCardProvider.dart';
-import 'package:handsfree/provider/newsFeedProvider.dart';
-import 'package:handsfree/provider/helpdeskProvider.dart';
-import 'package:handsfree/provider/lessonCardProvider.dart';
+import 'package:handsfree/provider/lessonCardPRovider.dart';
 import 'package:handsfree/provider/subLessonProvider.dart';
 import 'package:handsfree/screens/FeedBack/feedback.dart';
-import 'package:handsfree/screens/authenticate/authenticate.dart';
 import 'package:handsfree/screens/authenticate/signIn.dart';
 import 'package:handsfree/screens/authenticate/signUp.dart';
-import 'package:handsfree/screens/chat/chat.dart';
-import 'package:handsfree/screens/chat/chatHome.dart';
 import 'package:handsfree/screens/dictionary/dictionary.dart';
 import 'package:handsfree/screens/dictionary/translator.dart';
 import 'package:handsfree/screens/home/home.dart';
@@ -20,25 +13,17 @@ import 'package:handsfree/screens/learn/congrats.dart';
 import 'package:handsfree/screens/learn/learn.dart';
 import 'package:handsfree/screens/learn/mainLearningPage.dart';
 import 'package:handsfree/screens/learn/subLesson.dart';
-import 'package:handsfree/screens/news/news.dart';
 import 'package:handsfree/screens/profile/profile.dart';
 import 'package:handsfree/screens/profile/acknowledgement.dart';
 import 'package:handsfree/screens/settings/helpdesk.dart';
 import 'package:handsfree/screens/settings/settings.dart';
-import 'package:handsfree/screens/social/friendRequest.dart';
-import 'package:handsfree/screens/social/searchGlobal.dart';
-import 'package:handsfree/screens/social/social.dart';
 import 'package:handsfree/screens/settings/terms.dart';
+import 'package:handsfree/screens/social/social.dart';
 import 'package:handsfree/screens/wrapper.dart';
-import 'package:handsfree/services/ShPref.dart';
 import 'package:handsfree/services/auth.dart';
 import 'package:handsfree/provider/lessonProvider.dart';
-import 'package:handsfree/services/mediaAccess.dart';
-import 'package:handsfree/services/prepSendImage.dart';
-import 'package:handsfree/services/viewPic.dart';
 import 'package:handsfree/services/userPreference.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 import 'package:handsfree/models/newUser.dart';
 import 'theme/theme_manager.dart';
@@ -46,7 +31,6 @@ import 'theme/theme_constants.dart';
 
 // to store all available camera on device as global variable for easy access
 List<CameraDescription> camerasAvailable = [];
-
 Future<void> main() async {
   try {
     WidgetsFlutterBinding.ensureInitialized();
@@ -68,32 +52,9 @@ Future<void> main() async {
             create: (_) => SubLessonProvider()),
         ChangeNotifierProvider<LessonCardProvider>(
             create: (_) => LessonCardProvider()),
-        ChangeNotifierProvider<HelpDeskProvider>(
-            create: (_) => HelpDeskProvider()),
-        ChangeNotifierProvider<NewsFeedProvider>(
-            create: (_) => NewsFeedProvider()),
-        ChangeNotifierProvider<LessonCardProvider>(
-            create: (_) => LessonCardProvider()),
       ],
     ),
   );
-// =======
-//   runApp(MultiProvider(child: const MyApp(), providers: [
-//     ChangeNotifierProvider<LessonProvider>(create: (_) => LessonProvider()),
-//     ChangeNotifierProvider<SubLessonProvider>(
-//         create: (_) => SubLessonProvider()),
-//     ChangeNotifierProvider<LessonCardProvider>(
-//         create: (_) => LessonCardProvider()),
-//   ]));
-//   // runApp(ChangeNotifierProvider<LessonProvider>(
-//   //   create: (_) => LessonProvider(),
-//   //   child: const MyApp(),
-//   // ));
-//   //
-//   // await UserSimplePreferences.init();
-//   //
-//   // runApp(const MyApp());
-// >>>>>>> Stashed changes
 }
 
 ThemeManager _themeManager = ThemeManager();
@@ -108,14 +69,12 @@ class MyApp extends StatelessWidget {
       value: AuthService().user,
       initialData: null,
       child: MaterialApp(
-        debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: lightTheme,
         darkTheme: darkTheme,
         themeMode: _themeManager.themeMode,
         home: const Wrapper(),
         routes: {
-          "/auth": (context) => const Authenticate(),
           "/auth/signIn": (context) => const SignIn(),
           "/auth/signUp": (context) => const SignUp(),
           "/social": (context) => const Social(),
@@ -132,14 +91,6 @@ class MyApp extends StatelessWidget {
           // "/translator": (context) => const Translator(),
           "/helpCenter": (context) => const HelpDesk(),
           "/feedback": (context) => const FeedBack(),
-          "/news": (context) => const News(),
-          "/chatHome": (context) => ChatHome(),
-          "/chatHome/chat": (context) => Chat(),
-          "/camera": (context) => CameraScreen(),
-          "/prepsend": (context) => PrepSendImage(),
-          "/viewPic": (context) => ViewPic(),
-          "/viewFriendRequest": (context) => FriendRequest(),
-          "/searchGlobalUsers": (context) => SearchGlobal(),
         },
       ),
     );
