23ca5c77eff30a8c18b5656e570f68ead63019e7
KuckSN <93997126+KuckSN@users.noreply.github.com>
2022-03-17 14:56:25 +0800

    Fixed Learning Issue
    Done

--- lib/screens/learn/learn.dart
+++ lib/screens/learn/learn.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:handsfree/screens/learn/subLesson.dart';
 import 'package:handsfree/services/database.dart';
 import 'package:handsfree/widgets/buildButton.dart';
@@ -32,178 +33,196 @@ class _LearnState extends State<Learn> {
     }
     super.initState();
   }
-
+  DateTime timeBackPressed = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
 
     final user = Provider.of<NewUser?>(context);
 
-    return StreamBuilder<List<LessonModel>?>(
-      stream: DatabaseService(uid: user!.uid).getSyllabusOverview(),
-      builder: (context, snapshot){
-        if(snapshot.hasData){
+    return WillPopScope(
+      onWillPop: () async{
+        final difference = DateTime.now().difference(timeBackPressed);
+        final isExitWarning = difference >= Duration(milliseconds: 500);
 
-          List<LessonModel>? syllabusOverview = snapshot.data;
-          if(syllabusOverview!.isNotEmpty){
-            Provider.of<LessonProvider>(context, listen: false)
-                .setLessons(syllabusOverview);
-          }
+        timeBackPressed = DateTime.now();
 
-          int total = syllabusOverview.length;
-          int i = 0;
-          for(LessonModel each in syllabusOverview){
-            if(each.isCompleted==true){
-              i++;
-            }
-            else{
-              //do nothing
-            }
-          }
-          progress = i/total;
+        if(isExitWarning){
+          final message = 'Press back again to exit';
+          Fluttertoast.showToast(msg: message, fontSize: 18);
 
-          return Scaffold(
-            body: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    alignment: Alignment.topCenter,
-                    image: AssetImage('assets/image/purple_heading.png'),
-                    fit: BoxFit.cover),
-              ),
-              child: Container(
-                padding: const EdgeInsets.only(left: 40, bottom: 5, right: 40),
-                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 10),
-                child: ListView(
-                  physics: NeverScrollableScrollPhysics(),
-                  children: [
-                    Container(
-                      // margin: const EdgeInsets.only(left: 16, right: 16, top: 16),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Basic Text',
-                                style: GoogleFonts.montserrat(
-                                  fontSize: 25,
-                                  fontWeight: FontWeight.w800,
-                                  color: kTextLight,
-                                ),
-                              ),
-                              const Padding(
-                                padding: EdgeInsets.only(bottom: 5),
-                              ),
-                              Text(
-                                (progress * 100).toString() + '% completed',
-                                style: GoogleFonts.montserrat(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w600,
-                                  color: kTextLight,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Container(
-                            alignment: Alignment.centerRight,
-                            width: 60,
-                            height: 60,
-                            child: Stack(children: <Widget>[
-                              Image.asset(
-                                'assets/image/large_progress_bar.png',
-                                scale: 4,
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(7),
-                                child: CircularPercentIndicator(
-                                  radius: 22.0,
-                                  lineWidth: 5.0,
-                                  percent: progress,
-                                  // center: new Text("100%"),
-                                  progressColor: kOrangeMid,
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(13.0),
-                                child: Image.asset(
-                                  'assets/image/small_progress_bar.png',
-                                  scale: 4,
-                                ),
-                              ),
-                            ]),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 60),
-                    ),
-                    ShaderMask(
-                      shaderCallback: (Rect rect) {
-                        return const LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                            Colors.purple,
-                            Colors.transparent,
-                            Colors.transparent,
-                            Colors.purple
-                          ],
-                          stops: [
-                            0.0,
-                            0.05,
-                            0.95,
-                            1.0
-                          ], // 10% purple, 80% transparent, 10% purple
-                        ).createShader(rect);
-                      },
-                      blendMode: BlendMode.dstOut,
-                      child: Consumer<LessonProvider>(
-                          builder: (context, providerLesson, child) {
-                            var lessons = providerLesson.lessons;
-
-                            return Container(
-                              height: MediaQuery.of(context).size.height / 1.7,
-                              child: ListView.builder(
-                                physics: BouncingScrollPhysics(),
-                                scrollDirection: Axis.vertical,
-                                padding: EdgeInsets.symmetric(
-                                    horizontal: MediaQuery.of(context).size.width / 8),
-                                itemCount: lessons.length,
-                                itemBuilder: (context, index) {
-                                  return GestureDetector(
-                                    onTap: () {
-                                      Provider.of<LessonProvider>(context, listen: false)
-                                          .setClickLesson(lessons[index]);
-                                      // LessonModel lesson = context.read<LessonProvider>().getClickedLesson;
-                                      // print(lesson.lessonId);
-                                      // Navigator.pushNamed(context, PageTransition(type: PageTransitionType.leftToRight, child: const SubLevel()));
-                                      Navigator.pushNamed(context, "/sublevel");
-                                    },
-                                    child: ColumnList(
-                                      lesson: lessons[index],
-                                    ),
-                                  );
-                                },
-                              ),
-                            );
-                          }),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            floatingActionButton: NavBar.Buttons(context),
-            floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-            extendBody: true,
-            bottomNavigationBar: NavBar.bar(context, 2),
-          );
-        }else{
-          return Loading();
+          return false;
+        } else {
+          Fluttertoast.cancel();
+          return true;
         }
       },
+      child: StreamBuilder<List<LessonModel>?>(
+        stream: DatabaseService(uid: user!.uid).getSyllabusOverview(),
+        builder: (context, snapshot){
+          if(snapshot.hasData){
+
+            List<LessonModel>? syllabusOverview = snapshot.data;
+            if(syllabusOverview!.isNotEmpty){
+              Provider.of<LessonProvider>(context, listen: false)
+                  .setLessons(syllabusOverview);
+            }
+
+            int total = syllabusOverview.length;
+            int i = 0;
+            for(LessonModel each in syllabusOverview){
+              if(each.isCompleted==true){
+                i++;
+              }
+              else{
+                //do nothing
+              }
+            }
+            progress = i/total;
+
+            return Scaffold(
+              body: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      alignment: Alignment.topCenter,
+                      image: AssetImage('assets/image/purple_heading.png'),
+                      fit: BoxFit.cover),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.only(left: 40, bottom: 5, right: 40),
+                  margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 10),
+                  child: ListView(
+                    physics: NeverScrollableScrollPhysics(),
+                    children: [
+                      Container(
+                        // margin: const EdgeInsets.only(left: 16, right: 16, top: 16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Basic Text',
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w800,
+                                    color: kTextLight,
+                                  ),
+                                ),
+                                const Padding(
+                                  padding: EdgeInsets.only(bottom: 5),
+                                ),
+                                Text(
+                                  (progress * 100).toString() + '% completed',
+                                  style: GoogleFonts.montserrat(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    color: kTextLight,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Container(
+                              alignment: Alignment.centerRight,
+                              width: 60,
+                              height: 60,
+                              child: Stack(children: <Widget>[
+                                Image.asset(
+                                  'assets/image/large_progress_bar.png',
+                                  scale: 4,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(7),
+                                  child: CircularPercentIndicator(
+                                    radius: 22.0,
+                                    lineWidth: 5.0,
+                                    percent: progress,
+                                    // center: new Text("100%"),
+                                    progressColor: kOrangeMid,
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(13.0),
+                                  child: Image.asset(
+                                    'assets/image/small_progress_bar.png',
+                                    scale: 4,
+                                  ),
+                                ),
+                              ]),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(bottom: 60),
+                      ),
+                      ShaderMask(
+                        shaderCallback: (Rect rect) {
+                          return const LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.purple,
+                              Colors.transparent,
+                              Colors.transparent,
+                              Colors.purple
+                            ],
+                            stops: [
+                              0.0,
+                              0.05,
+                              0.95,
+                              1.0
+                            ], // 10% purple, 80% transparent, 10% purple
+                          ).createShader(rect);
+                        },
+                        blendMode: BlendMode.dstOut,
+                        child: Consumer<LessonProvider>(
+                            builder: (context, providerLesson, child) {
+                              var lessons = providerLesson.lessons;
+
+                              return Container(
+                                height: MediaQuery.of(context).size.height / 1.7,
+                                child: ListView.builder(
+                                  physics: BouncingScrollPhysics(),
+                                  scrollDirection: Axis.vertical,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: MediaQuery.of(context).size.width / 8),
+                                  itemCount: lessons.length,
+                                  itemBuilder: (context, index) {
+                                    return GestureDetector(
+                                      onTap: () {
+                                        Provider.of<LessonProvider>(context, listen: false)
+                                            .setClickLesson(lessons[index]);
+                                        // LessonModel lesson = context.read<LessonProvider>().getClickedLesson;
+                                        // print(lesson.lessonId);
+                                        // Navigator.pushNamed(context, PageTransition(type: PageTransitionType.leftToRight, child: const SubLevel()));
+                                        Navigator.pushNamed(context, "/sublevel");
+                                      },
+                                      child: ColumnList(
+                                        lesson: lessons[index],
+                                      ),
+                                    );
+                                  },
+                                ),
+                              );
+                            }),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              floatingActionButton: NavBar.Buttons(context),
+              floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+              extendBody: true,
+              bottomNavigationBar: NavBar.bar(context, 2),
+            );
+          }else{
+            return Loading();
+          }
+        },
+      ),
     );
   }
 }
