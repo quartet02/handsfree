545c92299b086c081fb0699ad0ce939a443ed8be
mingng18 <92716196+mingng18@users.noreply.github.com>
2022-03-29 23:00:41 +0800

    Fix Loading

--- lib/widgets/loadingWholeScreen.dart
+++ lib/widgets/loadingWholeScreen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:handsfree/widgets/constants.dart';
 
 class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.purple[100],
+      color: Colors.white,
       child: const Center(
         child: SpinKitChasingDots(
-          color: Colors.purple,
+          color: kPurpleLight,
           size: 50.0,
         ),
       ),
