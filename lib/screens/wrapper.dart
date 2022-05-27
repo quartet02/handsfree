a434fa604dcfd9e51e009a24dfa9de61d1c17ad3
Programmer420-1 <limweixin17@gmail.com>
2022-05-27 22:08:19 +0800

    Done forget password fx

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:handsfree/models/newUser.dart';
 import 'package:handsfree/screens/authenticate/authenticate.dart';
 import 'package:handsfree/screens/home/home.dart';
+import 'package:handsfree/screens/learn/learn.dart';
 import 'package:handsfree/services/firebase_messaging_service.dart';
 import 'package:provider/provider.dart';
 
@@ -18,7 +19,7 @@ class Wrapper extends StatelessWidget {
       return const Authenticate();
     } else {
       FirebaseMessagingService.updateToken(user.uid!);
-      return const Home();
+      return const Learn();
     }
   }
 }
